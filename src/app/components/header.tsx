bca2e0da4bc696f4d7925115dec4866e54d688f1
elsong86 <ellissong@gmail.com>
2024-07-27 20:07:51 -0700

    created a header component, home page, and about page

---
+++ src/app/components/header.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <nav>
+                <div>
+                    <Link href="/">Home</Link>
+                </div>
+                <div>
+                    <Link href="/about">About</Link>
+                </div>
+                <div>
+                    <Link href="/projects">Projects</Link>
+                </div>
+                <div>
+                    <Link href="/contact">Contact</Link>
+                </div>
+                </nav>
+        </header>
+    )
+}
+
+export default Header; 
